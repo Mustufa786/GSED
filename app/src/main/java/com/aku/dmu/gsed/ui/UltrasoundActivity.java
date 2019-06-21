d36d95782d115237cd78fa97dac5d444a4c44f33
Mustufa786 <mustufa_ayub@hotmail.com>
2019-06-21 11:46:48 +0500

    Ultrasound layout done

---
+++ app/src/main/java/com/aku/dmu/gsed/ui/UltrasoundActivity.java
@@ -0,0 +1,45 @@
+package com.aku.dmu.gsed.ui;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.aku.dmu.gsed.JSONGenerator.GeneratorClass;
+import com.aku.dmu.gsed.R;
+import com.aku.dmu.gsed.databinding.ActivityUltrasoundBinding;
+
+import org.json.JSONObject;
+
+public class UltrasoundActivity extends AppCompatActivity {
+
+    ActivityUltrasoundBinding bi;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_ultrasound);
+        bi.setCallback(this);
+    }
+
+    public void BtnEnd() {
+
+    }
+
+    private void SaveDraft() {
+
+    }
+
+    private boolean formValidation() {
+
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(this, "You can't go back", Toast.LENGTH_SHORT).show();
+    }
+
+}
