0c96517b33c9e28f2c48b98409eafd21867aa5d9
Mustufa786 <mustufa_ayub@hotmail.com>
2019-06-24 12:00:14 +0500

    strings file done

--- app/src/main/java/com/aku/dmu/gsed/ui/ultrasound/UltrasoundActivity.java
+++ app/src/main/java/com/aku/dmu/gsed/ui/ultrasound/UltrasoundActivity.java
@@ -3,9 +3,14 @@ package com.aku.dmu.gsed.ui.ultrasound;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.aku.dmu.gsed.R;
 import com.aku.dmu.gsed.databinding.ActivityUltrasoundBinding;
+import com.aku.dmu.gsed.jsonGenerator.GeneratorClass;
+import com.aku.dmu.gsed.validations.ValidatorClass;
+
+import org.json.JSONObject;
 
 public class UltrasoundActivity extends AppCompatActivity {
 
@@ -23,13 +28,34 @@ public class UltrasoundActivity extends AppCompatActivity {
 
     }
 
-    private void saveDraft() {
+    public void BtnContinue() {
+        if (!formValidation())
+            return;
+
+        SaveDraft();
+        if (UpdateDB()) {
+
+        } else {
+            Toast.makeText(this, "Error in updating db!!", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    private boolean UpdateDB() {
+
+        return true;
+    }
+
+    private void SaveDraft() {
+
+        JSONObject json = GeneratorClass.getContainerJSON(bi.fldGrpUltrasound, true);
 
     }
 
     private boolean formValidation() {
 
-        return true;
+        return ValidatorClass.EmptyCheckingContainer(this, bi.fldGrpUltrasound);
     }
 
 }
