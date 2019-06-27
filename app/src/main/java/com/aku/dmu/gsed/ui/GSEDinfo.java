954775b9c7a7b7510c62a4524f30854248da401e
Mustufa786 <mustufa_ayub@hotmail.com>
2019-06-27 17:30:17 +0500

    layout issue in info done

--- app/src/main/java/com/aku/dmu/gsed/ui/GSEDinfo.java
+++ app/src/main/java/com/aku/dmu/gsed/ui/GSEDinfo.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 import com.aku.dmu.gsed.R;
@@ -25,8 +26,22 @@ public class GSEDinfo extends AppCompatActivity {
         bi = DataBindingUtil.setContentView(this, R.layout.activity_gsed_info);
         bi.setCallback(this);
 
+        setTitle(R.string.gsed_info);
+
         formType = getIntent().getStringExtra(Constants.formType);
 
+        setListeners();
+
+    }
+
+    private void setListeners() {
+
+        bi.gsedCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bi.checkLayout.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
     public boolean formValidation() {
